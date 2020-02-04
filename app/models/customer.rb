e0d1e44077a948102d9bce654c6a9bf2ae2c1fbe
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-02-04 14:45:39 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
 class Customer
+
   attr_accessor :given_name, :family_name
+
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def restaurants
+    Review.all.select do |reviews|
+      reviews.customer == self
+    end.uniq
+  end 
+
+  def add_review(restaurant,rating)
+    Review.new(self,restaurant,rating)
+  end
+
+  def num_reviews
+    Review.all.select {|reviews| reviews.customer == self}.count
+  end
+
+  def self.find_by_name(fname)
+      self.all.find do |customer|
+        customer.full_name == fname
+    end
+  end
+
+  def self.find_all_by_given_name(gname)
+    self.all.select do |customer|
+      customer.given_name == gname
+    end
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,18 @@
 class Review
-  
+
+    attr_reader :customer, :rating, :restaurant
+
+    @@all = []
+
+    def initialize(customer,restaurant,rating)
+        @customer = customer
+        @rating = rating
+        @restaurant = restaurant
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
