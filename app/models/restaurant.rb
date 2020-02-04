0b1424f58f24366f1067b92dabf707874e728005
Fika98 <Afzalov98@gmail.com>
2020-02-04 14:47:06 -0500

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,30 @@
 class Restaurant
-  attr_reader :name
+  @@all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def name
+    @all.map{|restaurant|restaurant.name} 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def reviews
+    Review.all.select{|review| review.restaurant}
+  end 
+
+  def customers 
+    Customer.all.select{|customers|customers.rate == self}uniq 
+  end 
+
+  def average_star_rating
+    @@all.reduce(0){|sum,n| sum + n.rate.to_f / self.rate.length}
+  end 
 
 end
