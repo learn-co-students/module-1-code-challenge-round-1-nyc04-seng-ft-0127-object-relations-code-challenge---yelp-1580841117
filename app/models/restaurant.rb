d49d85f2f8d38d560bcd5905bcab3a326540843b
kenmho38 <kenmho38@hotmail.com>
2020-02-04 14:46:13 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,27 @@
 class Restaurant
+  
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+  review.all.select do |restaurant|
+    review.restaurant == self
+  end
+
+  def customers
+    review.map do |restaurant|
+      reviews.customers
+    end
   end
 
 end
