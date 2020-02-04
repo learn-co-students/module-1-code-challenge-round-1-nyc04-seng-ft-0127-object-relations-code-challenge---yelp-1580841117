0330af885f740b4beb36eb4822d8bacd81b7aa12
abidtalukdar <abiditalukdar@gmail.com>
2020-02-04 14:46:31 -0500

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
   attr_accessor :given_name, :family_name
+  ALL = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    ALL << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all 
+    ALL
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def my_reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants
+    my_reviews.map do |review|
+      review.restaurant
+    end.uniq # sorry, I know this looks ugly
+  end
+
+  def num_reviews
+    my_reviews.length
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |customer|
+      customer.full_name == name
+    end
+  end
+
+  def self.find_all_by_given_name(name)
+    self.all.select do |customer|
+      customer.given_name  == name
+    end
+  end
+
 end
